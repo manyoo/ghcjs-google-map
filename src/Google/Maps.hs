aab100fde7b7435cb33b3cb6f6adf6e5fd7e7843
Eric Wong <ericsyw@gmail.com>
2015-10-28 08:50:17 +0800

    Add a Google.Maps module to expose all other modules

---
+++ src/Google/Maps.hs
@@ -0,0 +1,13 @@
+module Google.Maps (
+    module Google.Maps.Types,
+    module Google.Maps.LatLng,
+    module Google.Maps.Event,
+    module Google.Maps.Map,
+    module Google.Maps.Geocoder
+    ) where
+
+import Google.Maps.Types
+import Google.Maps.LatLng
+import Google.Maps.Event
+import Google.Maps.Map
+import Google.Maps.Geocoder
