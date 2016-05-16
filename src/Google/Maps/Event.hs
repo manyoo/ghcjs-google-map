9864e28002ff82f7dfd09d244df7ceff3cb93023
Eric Wong <ericsyw@gmail.com>
2016-05-16 15:15:09 +0800

    Fix a variable name

--- src/Google/Maps/Event.hs
+++ src/Google/Maps/Event.hs
@@ -14,9 +14,9 @@ foreign import javascript unsafe "google.maps.event.addListener($1, $2, $3, $4)"
     jsAddListener :: Map -> JSString -> Callback (IO ()) -> Bool -> IO MapsEventListener
 
 addListener :: Map -> JSString -> IO () -> Bool -> IO MapsEventListener
-addListener map evtName cb capture = do
+addListener m evtName cb capture = do
     jsCb <- syncCallback ThrowWouldBlock cb
-    jsAddListener map evtName jsCb capture
+    jsAddListener m evtName jsCb capture
 
 foreign import javascript unsafe "google.maps.event.removeListener($1)"
     removeListener :: MapsEventListener -> IO ()
